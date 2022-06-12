37a002517d2a732c6d31f65385c8fd937d105f1e
Justin Townley <justin.d.townley@gmail.com>
2022-06-11 23:25:36 -0700

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split('');
+    arr.reverse();
+    return arr.join('');
 };
 
 // Do not edit below this line
