adc4e1d84c1f05fa4cd6933967042759b0c86e76
Justin Townley <justin.d.townley@gmail.com>
2022-06-12 23:37:03 -0700

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) { 
+    let small = 0;
+    let large = 0;
+    let result = 0;
+
+    if (typeof num1 !== 'number'|| typeof num2 !== 'number' || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    if (num1 < num2) {
+        small = num1;
+        large = num2;        
+    } else {
+        small = num2;
+        large = num1;
+    }
+
+    for (let i = small; i <= large; i++) {
+        result += i;
+    }
+    return result;
 
 };
 
