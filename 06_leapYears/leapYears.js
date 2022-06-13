30a5941d0a7637dc636d2ef3830d074bde287c87
Justin Townley <justin.d.townley@gmail.com>
2022-06-12 23:48:32 -0700

    Finish exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let result = false;
 
+    if (year % 4 === 0) {
+        result = true;
+    }
+    if (year % 100 === 0 && year % 400 !== 0) {
+        result = false;
+    }
+    return result;
 };
 
 // Do not edit below this line
