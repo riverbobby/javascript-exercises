69003c891744f06719233ca17e6616d82a38fc57
Justin Townley <justin.d.townley@gmail.com>
2022-06-18 00:24:15 -0700

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /[a-z]/g;
+    arr = string.toLowerCase()
+        .match(regex);
+    let i = 0;
+    while (i < arr.length / 2) {
+        if (arr[i] !== arr[arr.length - i -1]) {
+            return false;
+        }
+        i++;
+    }
+    return true;
 };
 
 // Do not edit below this line
