241ace3aac02a80e72cc12402a6d3687f6a00a58
Justin Townley <justin.d.townley@gmail.com>
2022-06-17 23:11:13 -0700

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, num) => total+= num , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total*= num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  result = 1;
+  if (num < 1) return result;
+  for (let i = 1; i <= num; i++) {
+    result*= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
