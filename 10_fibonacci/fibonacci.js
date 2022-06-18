ff2afa43161626b6671b5b0982f0d2ce7c1d5079
Justin Townley <justin.d.townley@gmail.com>
2022-06-18 01:30:30 -0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = +num;
+    if (fib < 1) return 'OOPS';
+    prevNum = 1;
+    result = 1;//
+    for (let i = 3; i <= fib; i++) {
+        let tempNum = result;
+        result = result + prevNum;
+        prevNum = tempNum;
+    }
+    return result;
 };
 
 // Do not edit below this line
