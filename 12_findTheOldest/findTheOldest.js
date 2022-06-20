2d5ab2d1264a143937648bef4c8a79b4e972719d
Justin Townley <justin.d.townley@gmail.com>
2022-06-20 12:26:28 -0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sorted = [...arr];
+    sorted.sort((a, b) => {
+        let aEnd = 0;
+        let bEnd = 0;
+        if (a.yearOfDeath === undefined) {
+            aEnd = new Date().getFullYear();
+        }else {
+            aEnd = a.yearOfDeath;
+        }
+        if (b.yearOfDeath === undefined) {
+            bEnd = new Date().getFullYear();
+        }else {
+            bEnd = b.yearOfDeath;
+        }
+        return ((bEnd - b.yearOfBirth) - (aEnd - a.yearOfBirth));
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
